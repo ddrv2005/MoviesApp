f31b50ea50550a3c8b21fe125cab364d78a5527d
Dickson Rojas <ddrv20co@gmail.com>
2021-04-20 23:18:14 +0200

    Navigation to movie detail

--- app/src/main/java/com/example/moviesapp/di/ViewModelsModule.kt
+++ app/src/main/java/com/example/moviesapp/di/ViewModelsModule.kt
@@ -1,9 +1,11 @@
 package com.example.moviesapp.di
 
+import com.example.moviesapp.viewmodels.MovieDetailViewModel
 import com.example.moviesapp.viewmodels.PopularMoviesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
     viewModel { PopularMoviesViewModel() }
+    viewModel { MovieDetailViewModel() }
 }
--- app/src/main/java/com/example/moviesapp/fragments/MovieDetailFragment.kt
+++ app/src/main/java/com/example/moviesapp/fragments/MovieDetailFragment.kt
@@ -5,10 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
 import com.example.moviesapp.databinding.FragmentMovieDetailBinding
+import com.example.moviesapp.models.MovieDetail
+import com.example.moviesapp.utils.hide
+import com.example.moviesapp.utils.loadImage
+import com.example.moviesapp.utils.show
+import com.example.moviesapp.viewmodels.MovieDetailActions
+import com.example.moviesapp.viewmodels.MovieDetailViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MovieDetailFragment: Fragment() {
 
+    private val movieDetailViewModel: MovieDetailViewModel by viewModel()
+    private val arguments: MovieDetailFragmentArgs by navArgs()
     private var _binding: FragmentMovieDetailBinding? = null
     private val binding get() = _binding!!
 
@@ -20,8 +31,33 @@ class MovieDetailFragment: Fragment() {
         _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+    override fun onResume() {
+        super.onResume()
+        movieDetailViewModel.movieDetailActions.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                MovieDetailActions.OnLoading -> binding.progressBar.show()
+                is MovieDetailActions.OnMovieDetailRetrieved -> loadInfo(it.movieDetail)
+            }
+        })
+        if (arguments.movieId != -1) {
+            movieDetailViewModel.requestPopularMovies(arguments.movieId)
+        } else {
+            showError()
+        }
+    }
+
+    private fun loadInfo(movieDetail: MovieDetail) {
+        binding.progressBar.hide()
+        movieDetail.posterPath?.let {
+            binding.imageViewMovieDetail.loadImage(it)
+        }
+
+        binding.textViewNameDatail.text = movieDetail.title
+        binding.textViewMoviesDetail.text = movieDetail.overview
+    }
+
+    private fun showError() {
 
     }
 }
